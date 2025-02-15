942acc59111056bdc0c2dae9b4fb4c8a1b0b070a
CGengineer <kororin0319@outlook.jp>
2025-02-15 18:36:36 +0900

    はむはむさんへバレンタインプレゼントでございます

---
+++ marubatuGame/marubatuGame/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace marubatuGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn13 = new System.Windows.Forms.Button();
+            this.btn23 = new System.Windows.Forms.Button();
+            this.btn33 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn22 = new System.Windows.Forms.Button();
+            this.btn32 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn21 = new System.Windows.Forms.Button();
+            this.btn31 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn13
+            // 
+            this.btn13.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn13.Location = new System.Drawing.Point(12, 12);
+            this.btn13.Name = "btn13";
+            this.btn13.Size = new System.Drawing.Size(100, 100);
+            this.btn13.TabIndex = 0;
+            this.btn13.UseVisualStyleBackColor = true;
+            this.btn13.Click += new System.EventHandler(this.AllButton_Click);
+            // 
+            // btn23
+            // 
+            this.btn23.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn23.Location = new System.Drawing.Point(118, 12);
+            this.btn23.Name = "btn23";
+            this.btn23.Size = new System.Drawing.Size(100, 100);
+            this.btn23.TabIndex = 1;
+            this.btn23.UseVisualStyleBackColor = true;
+            this.btn23.Click += new System.EventHandler(this.AllButton_Click);
+            // 
+            // btn33
+            // 
+            this.btn33.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn33.Location = new System.Drawing.Point(224, 12);
+            this.btn33.Name = "btn33";
+            this.btn33.Size = new System.Drawing.Size(100, 100);
+            this.btn33.TabIndex = 2;
+            this.btn33.UseVisualStyleBackColor = true;
+            this.btn33.Click += new System.EventHandler(this.AllButton_Click);
+            // 
+            // btn12
+            // 
+            this.btn12.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn12.Location = new System.Drawing.Point(12, 118);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(100, 100);
+            this.btn12.TabIndex = 3;
+            this.btn12.UseVisualStyleBackColor = true;
+            this.btn12.Click += new System.EventHandler(this.AllButton_Click);
+            // 
+            // btn22
+            // 
+            this.btn22.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn22.Location = new System.Drawing.Point(118, 118);
+            this.btn22.Name = "btn22";
+            this.btn22.Size = new System.Drawing.Size(100, 100);
+            this.btn22.TabIndex = 4;
+            this.btn22.UseVisualStyleBackColor = true;
+            this.btn22.Click += new System.EventHandler(this.AllButton_Click);
+            // 
+            // btn32
+            // 
+            this.btn32.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn32.Location = new System.Drawing.Point(224, 118);
+            this.btn32.Name = "btn32";
+            this.btn32.Size = new System.Drawing.Size(100, 100);
+            this.btn32.TabIndex = 5;
+            this.btn32.UseVisualStyleBackColor = true;
+            this.btn32.Click += new System.EventHandler(this.AllButton_Click);
+            // 
+            // btn11
+            // 
+            this.btn11.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn11.Location = new System.Drawing.Point(12, 224);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(100, 100);
+            this.btn11.TabIndex = 8;
+            this.btn11.UseVisualStyleBackColor = true;
+            this.btn11.Click += new System.EventHandler(this.AllButton_Click);
+            // 
+            // btn21
+            // 
+            this.btn21.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn21.Location = new System.Drawing.Point(118, 224);
+            this.btn21.Name = "btn21";
+            this.btn21.Size = new System.Drawing.Size(100, 100);
+            this.btn21.TabIndex = 7;
+            this.btn21.UseVisualStyleBackColor = true;
+            this.btn21.Click += new System.EventHandler(this.AllButton_Click);
+            // 
+            // btn31
+            // 
+            this.btn31.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn31.Location = new System.Drawing.Point(223, 224);
+            this.btn31.Name = "btn31";
+            this.btn31.Size = new System.Drawing.Size(100, 100);
+            this.btn31.TabIndex = 6;
+            this.btn31.UseVisualStyleBackColor = true;
+            this.btn31.Click += new System.EventHandler(this.AllButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 340);
+            this.Controls.Add(this.btn11);
+            this.Controls.Add(this.btn21);
+            this.Controls.Add(this.btn31);
+            this.Controls.Add(this.btn32);
+            this.Controls.Add(this.btn22);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn33);
+            this.Controls.Add(this.btn23);
+            this.Controls.Add(this.btn13);
+            this.Name = "Form1";
+            this.Text = "○✖ゲーム";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn13;
+        private System.Windows.Forms.Button btn23;
+        private System.Windows.Forms.Button btn33;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn22;
+        private System.Windows.Forms.Button btn32;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn21;
+        private System.Windows.Forms.Button btn31;
+    }
+}
+
---
+++ marubatuGame/marubatuGame/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace marubatuGame
+{
+    public partial class Form1 : Form
+    {
+        public Space[,] _Spaces = new Space[3, 3];
+
+        public List<Space> _SpaceList = new List<Space>();
+        public enum Space
+        {
+            CrossSpace = 1,
+            CircleSpace = 2,
+            NoneSpace = 3,
+        }
+        public Space _TurnEnum = Space.CircleSpace;
+
+        public enum Direction
+        {
+            LeftUp = 1,
+            Up = 2,
+            RightUp = 3,
+            Left = 4,
+            Right = 5,
+            LeftDown = 6,
+            Down = 7,
+            RightDown = 8,
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int x = 0; x < _Spaces.GetLength(0); x++)
+            {
+                for (int y = 0; y < _Spaces.GetLength(1); y++)
+                {
+                    _Spaces[x, y] = Space.NoneSpace;
+                }
+            }
+        }
+
+        private void AllButton_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+
+            #region クリックしたボタンの位置情報を取得して配列に入れる
+            var btnPositionXY = button.Name.Substring(button.Name.Length - 2);
+            var btnPositionY = btnPositionXY.Substring(1, 1);
+            var btnPositionX = btnPositionXY.Substring(0, 1);
+
+            var btnPositionYInt = default(int);
+            if (int.TryParse(btnPositionY, out btnPositionYInt)) { }
+            var btnPositionXInt = default(int);
+            if (int.TryParse(btnPositionX, out btnPositionXInt)) { }
+
+
+            #endregion
+            btnPositionXInt = btnPositionXInt - 1;
+            btnPositionYInt = btnPositionYInt - 1;
+            _Spaces[btnPositionXInt, btnPositionYInt] = _TurnEnum;
+            aaa(btnPositionXInt, btnPositionYInt);
+
+            switch (_TurnEnum)
+            {
+                case Space.CircleSpace:
+
+                    button.Text = "○";
+                    _TurnEnum = Space.CrossSpace;
+                    break;
+
+                case Space.CrossSpace:
+                    button.Text = "✖";
+                    _TurnEnum = Space.CircleSpace;
+                    break;
+            }
+
+            if (_SpaceList.Count() >= 2)
+            {
+                switch (_TurnEnum)
+                {
+                    case Space.CircleSpace:
+                        MessageBox.Show("あなたの勝ちです", "黒の勝利", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        break;
+
+                    case Space.CrossSpace:
+                        MessageBox.Show("あなたの勝ちです", "白の勝利", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        break;
+                }
+
+                DialogResult result = MessageBox.Show("もう一度プレイしますか?", "Replay", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+
+
+                if (result == DialogResult.OK)
+                {
+                    foreach (Control control in this.Controls)
+                    {
+                        if (control is Button btn)
+                        {
+                            btn.Text = "";
+                        }
+                    }
+                    _SpaceList.Clear();
+
+                    for (int x = 0; x < _Spaces.GetLength(0); x++)
+                    {
+                        for (int y = 0; y < _Spaces.GetLength(1); y++)
+                        {
+                            _Spaces[x, y] = Space.NoneSpace;
+                        }
+                    }
+                    _TurnEnum = Space.CircleSpace;
+                }
+                else
+                {
+                    // キャンセルがクリックされた場合の処理
+                    MessageBox.Show("プレイを終了します。");
+                    Application.Exit(); // アプリケーションを終了
+                }
+
+            }
+        }
+        private void aaa(int X, int Y)
+        {
+            for (var direction = Direction.LeftUp; direction <= Direction.RightDown; direction++)
+            {
+                switch (direction)
+                {
+                    case Direction.LeftUp:
+                        for (int i = 1; i <= 2; i++)
+                        {
+                            var x = X - i;
+                            var y = Y + i;
+                            bbb(x, y);
+                        }
+                        break;
+
+                    case Direction.Up:
+                        for (int i = 1; i <= 2; i++)
+                        {
+                            var y = Y + i;
+                            bbb(X, y);
+                        }
+                        break;
+
+                    case Direction.RightUp:
+                        for (int i = 1; i <= 2; i++)
+                        {
+
+                            var x = X + i;
+                            var y = Y + i;
+
+                            bbb(x, y);
+                        }
+                        break;
+
+                    case Direction.Left:
+                        for (int i = 1; i <= 2; i++)
+                        {
+                            var x = X - i;
+                            bbb(x, Y);
+                        }
+                        break;
+
+                    case Direction.Right:
+                        for (int i = 1; i <= 2; i++)
+                        {
+                            var x = X + i;
+                            bbb(x, Y);
+                        }
+                        break;
+
+                    case Direction.LeftDown:
+                        for (int i = 1; i <= 2; i++)
+                        {
+                            var x = X - i;
+                            var y = Y - i;
+                            bbb(x, y);
+                        }
+                        break;
+
+                    case Direction.Down:
+                        for (int i = 1; i <= 2; i++)
+                        {
+                            var y = Y - i;
+                            bbb(X, y);
+                        }
+                        break;
+
+                    case Direction.RightDown:
+                        for (int i = 1; i <= 2; i++)
+                        {
+                            var x = X + i;
+                            var y = Y - i;
+                            bbb(x, y);
+                        }
+                        break;
+                }
+                if (_SpaceList.Count() >= 2)
+                {
+                    return;
+                }
+
+            }
+        }
+
+        private void bbb(int X, int Y)
+        {
+
+            //同じ色のものが指定した場所にあれば、続ける　なければループを終わる
+            if (X >= 0 &&
+                X <= 2 &&
+                Y >= 0 &&
+                Y <= 2)
+            {
+                if (_Spaces[X, Y] == _TurnEnum)
+                {
+                    _SpaceList.Add(_TurnEnum);
+                }
+                else
+                {
+                    _SpaceList.Clear();
+                }
+            }
+        }
+    }
+}
